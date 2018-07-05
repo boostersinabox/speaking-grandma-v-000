d615e276c3aa74761d0426e700a21c8638a22d97
Stacy Carr <stacy@boostersinabox.com>
2018-07-05 18:24:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,11 +16,9 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-  if phrase.upcase == (phrase)
-    return "NO, NOT SINCE 1938"
-  elseif 
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase == (phrase)
+    return "NO, NOT SINCE 1938"
   else
     return "HUH?! SPEAK UP, SONNY!"
-end
 end
